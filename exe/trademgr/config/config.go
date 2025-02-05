bc0749abf71426d2e21f26d742e6cc029137c09f
foo <yomohoo@gmail.com>
2025-02-05 21:00:58 +0800

    Add files via upload
    exe

---
+++ exe/trademgr/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Conf 全局配置对象
+var Conf *Config
+
+type RedisCfg struct {
+	IP          string `json:"ip" yaml:"ip"`
+	Port        int    `json:"port" yaml:"port"`
+	Password    string `json:"password" yaml:"password"`
+	DBIndex     int    `json:"dbIndex" yaml:"dbIndex"`
+	MaxIdle     int    `json:"maxIdle" yaml:"maxIdle"`
+	MaxActive   int    `json:"maxActive" yaml:"maxActive"`
+	IdleTimeout int    `json:"idleTimeout" yaml:"idleTimeout"`
+}
+
+// Logs 日记
+type LogCfg struct {
+	Path    string `yaml:"path"`
+	Level   string `yaml:"level"`
+	Type    string `yaml:"type"`
+	CutTime int    `yaml:"cutTime"`
+}
+
+type TradeKeyCfg struct {
+	APIKey    string `yaml:"apiKey"`
+	SecretKey string `yaml:"secretKey"`
+}
+
+type Config struct {
+	Redis     RedisCfg               `json:"redis" yaml:"redis"`
+	Log       LogCfg                 `json:"log" yaml:"log"`
+	TradeKeys map[string]TradeKeyCfg `json:"tradeKeys" yaml:"tradeKeys"`
+}
+
+func GetGlobalCfg() Config {
+	if Conf == nil {
+		err := InitConf(nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return *Conf
+}
+
+// InitConf 初始化配置文件
+func InitConf(path *string) error {
+	cfgPath := ""
+	if path != nil {
+		cfgPath = *path
+	} else {
+		wd, _ := os.Getwd()
+		cfgPath = wd + "/config/config.yaml"
+	}
+
+	flieContent, err := os.ReadFile(cfgPath)
+	if err != nil {
+		return err
+	}
+	fmt.Println("config file path:", string(flieContent))
+	err = yaml.Unmarshal(flieContent, &Conf)
+	return err
+}
