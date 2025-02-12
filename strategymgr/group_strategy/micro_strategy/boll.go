806607839961b51f81269370d410e8bc80e69a18
foo <yomohoo@gmail.com>
2025-02-13 00:11:29 +0800

    Delete strategymgr directory

--- strategymgr/group_strategy/micro_strategy/boll.go
+++
@@ -1,7 +0,0 @@
-package micro_strategy
-
-import common "freefi/strategymgr/common"
-
-func ExecuteBOLL(klines []common.KLine, params MicroStrategyParams) (ret *MicroStrategyRet, err error) {
-	return nil, nil
-}
--- strategymgr/group_strategy/micro_strategy/rsi.go
+++
@@ -1,7 +0,0 @@
-package micro_strategy
-
-import "freefi/strategymgr/common"
-
-func ExecuteRSI(klines []common.KLine, params MicroStrategyParams) (ret *MicroStrategyRet, err error) {
-	return nil, nil
-}
